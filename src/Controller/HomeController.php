1c5774a7e96e69fd598d105de49cdd21bbbaa9f9
Jeremy Legendre <jeremy.legendre.work@gmail.com>
2021-06-08 21:34:13 +0200

    Admin & crud init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
@@ -19,6 +19,24 @@ class HomeController extends AbstractController
         $articles = $repo->findAll();
         return $this->render("home/index.html.twig", [
             'articles' => $articles
+        ]);
+    }
+    
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show($id): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        
+        $article = $repo->find($id);
+
+        if (!$article) {
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render("show/index.html.twig", [
+            'article' => $article
         ]);
     }
 }
